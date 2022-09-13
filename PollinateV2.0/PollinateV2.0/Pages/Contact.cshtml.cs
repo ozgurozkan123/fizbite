7b77304a43cb931c81ce802a12c999f33d41b1b7
fizbite <56540638+fizbite@users.noreply.github.com>
2022-09-13 11:50:09 +0100

    Add files via upload

---
+++ PollinateV2.0/PollinateV2.0/Pages/Contact.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PollinateV2._0.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public bool hasData = false;
+        public string firstName = "";
+        public string lastName = "";
+        public string message = "";
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            hasData = true;
+            firstName = Request.Form["firstname"];
+            lastName = Request.Form["lastname"];
+            message = Request.Form["message"];
+        }
+    }
+}
